49502d613f0f5f1b3bdbd818d074a92751830247
prasetya <prasetya2423@gmail.com>
2022-06-16 20:20:28 +0700

    fix tooltip

--- resources/views/dashboard/data-subkriteria.blade.php
+++ resources/views/dashboard/data-subkriteria.blade.php
@@ -38,7 +38,7 @@
                                     <td>
                                         <center>
                                             <span data-toggle="modal" data-target="#modalForm">
-                                                <button id="btn_verif" onclick="updateData('{{$row->id}}','{{$row->description}}','{{$row->nilai_bobot}}')" type="button" class="btn btn-inverse-success btn-rounded btn-icon" data-toggle="tooltip" data-placement="top" title="Ubah Data Produk">
+                                                <button id="btn_verif" onclick="updateData('{{$row->id}}','{{$row->description}}','{{$row->nilai_bobot}}')" type="button" class="btn btn-inverse-success btn-rounded btn-icon" data-toggle="tooltip" data-placement="top" title="Ubah Data Sub Kriteria ">
                                                     <i class="mdi mdi-pencil-box-outline"></i>
                                                 </button>
                                             </span>
